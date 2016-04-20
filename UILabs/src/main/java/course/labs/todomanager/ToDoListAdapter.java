1897b540e487cb83f54ebf77707ae6bb933963c3
kumar7rk <rohitkumarrk1992@gmail.com>
2016-04-20 17:05:21 +0930

    Update ToDoListAdapter.java
    Formatting of the date and time @ line 136

--- UILabs/src/main/java/course/labs/todomanager/ToDoListAdapter.java
+++ UILabs/src/main/java/course/labs/todomanager/ToDoListAdapter.java
@@ -133,7 +133,7 @@ public class ToDoListAdapter extends BaseAdapter {
 		// Hint - use ToDoItem.FORMAT.format(toDoItem.getDate()) to get date and time String
 
 		final TextView dateView = (TextView) itemLayout.findViewById(R.id.dateView);
-		dateView.setText(toDoItem.getDate().toString());
+		dateView.setText(ToDoItem.FORMAT.format(toDoItem.getDate()));
 
 		// Return the View you just created
 		return itemLayout;
